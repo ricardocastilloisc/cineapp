bca0f2c8fd262ef0f989940b454d7e24272143dc
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-07 17:51:20 -0500

    50. Clase de Servicio PeliculasServiceImpl

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -40,47 +40,7 @@ public class HomeController {
 
 	// Metodo para generar una lista de Objetos de Modelo (Pelicula)
 	private List<Pelicula> getLista() {
-		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-		List<Pelicula> lista = null;
-		try {
-			lista = new LinkedList<>();
-			Pelicula pelicula1 = new Pelicula();
-			pelicula1.setId(1);
-			pelicula1.setTitulo("Power Ranger");
-			pelicula1.setDuracion(120);
-			pelicula1.setClasificacion("B");
-			pelicula1.setGenero("Aventura");
-			pelicula1.setFechaEstreno(formatter.parse("10-06-2019"));
-			pelicula1.setEstatus("inactivo");
 
-			Pelicula pelicula2 = new Pelicula();
-			pelicula2.setId(2);
-			pelicula2.setTitulo("La bella y la bestía");
-			pelicula2.setDuracion(132);
-			pelicula2.setClasificacion("A");
-			pelicula2.setGenero("Infantil");
-			pelicula2.setFechaEstreno(formatter.parse("11-06-2019"));
-			pelicula2.setImagen("bella.png");
-
-			Pelicula pelicula3 = new Pelicula();
-			pelicula3.setId(3);
-			pelicula3.setTitulo("Contratiempo");
-			pelicula3.setDuracion(106);
-			pelicula3.setClasificacion("B");
-			pelicula3.setGenero("Thriller");
-			pelicula3.setFechaEstreno(formatter.parse("12-06-2019"));
-			pelicula3.setImagen("contratiempo.png");
-
-			lista.add(pelicula1);
-			lista.add(pelicula2);
-			lista.add(pelicula3);
-
-			return lista;
-		} catch (Exception e) {
-			// TODO: handle exception}
-			System.out.println("Error: " + e.getMessage());
-			return null;
-		}
 
 	}
 
---
+++ src/main/java/net/itinajero/app/service/PeliculasServiceImpl.java
@@ -0,0 +1,60 @@
+package net.itinajero.app.service;
+
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.itinajero.app.model.Pelicula;
+
+public class PeliculasServiceImpl implements IPeliculasService{
+	
+	private List<Pelicula> lista = null;
+
+	public PeliculasServiceImpl() {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		
+		try {
+			lista = new LinkedList<>();
+			Pelicula pelicula1 = new Pelicula();
+			pelicula1.setId(1);
+			pelicula1.setTitulo("Power Ranger");
+			pelicula1.setDuracion(120);
+			pelicula1.setClasificacion("B");
+			pelicula1.setGenero("Aventura");
+			pelicula1.setFechaEstreno(formatter.parse("10-06-2019"));
+			pelicula1.setEstatus("inactivo");
+
+			Pelicula pelicula2 = new Pelicula();
+			pelicula2.setId(2);
+			pelicula2.setTitulo("La bella y la bestía");
+			pelicula2.setDuracion(132);
+			pelicula2.setClasificacion("A");
+			pelicula2.setGenero("Infantil");
+			pelicula2.setFechaEstreno(formatter.parse("11-06-2019"));
+			pelicula2.setImagen("bella.png");
+
+			Pelicula pelicula3 = new Pelicula();
+			pelicula3.setId(3);
+			pelicula3.setTitulo("Contratiempo");
+			pelicula3.setDuracion(106);
+			pelicula3.setClasificacion("B");
+			pelicula3.setGenero("Thriller");
+			pelicula3.setFechaEstreno(formatter.parse("12-06-2019"));
+			pelicula3.setImagen("contratiempo.png");
+
+			lista.add(pelicula1);
+			lista.add(pelicula2);
+			lista.add(pelicula3);
+		} catch (Exception e) {
+			// TODO: handle exception}
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+
+	@Override
+	public List<Pelicula> buscarTodas() {
+		// TODO Auto-generated method stub
+		return lista;
+	}
+
+}
