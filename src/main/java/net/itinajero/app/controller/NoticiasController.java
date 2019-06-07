f10715c9b5abb2fc53e9adeac8605de5639665ef
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-07 17:26:12 -0500

    47. Clase de modelo Noticia

--- src/main/java/net/itinajero/app/controller/NoticiasController.java
+++ src/main/java/net/itinajero/app/controller/NoticiasController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.itinajero.app.model.Noticia;
+
 @Controller
 @RequestMapping("/noticias")
 public class NoticiasController {
@@ -18,10 +20,13 @@ public class NoticiasController {
 	@PostMapping(value="/save")
 	public String guardar(@RequestParam("titulo") String titulo, @RequestParam("estatus") String estatus,
 			@RequestParam("detalle") String detalle) {
-			
-		System.out.println("Titulo: "+ titulo);
-		System.out.println("Estatus: "+ estatus);
-		System.out.println("Detalle: "+ detalle);
+		
+		Noticia noticia = new Noticia();
+		noticia.setTitulo(titulo);
+		noticia.setDetalle(detalle);
+		noticia.setEstatus(estatus);
+		//Pendiente: Guardar el objeo noticia en la BD5
+		System.out.println(noticia);
 		return "noticias/formNoticia";
 	}
 }
