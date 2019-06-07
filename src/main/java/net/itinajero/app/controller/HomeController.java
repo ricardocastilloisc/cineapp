e5e58c7af020cc0680460b335918e8b0d9225141
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-07 13:58:52 -0500

    33. Anotación @PathVariable - Ejemplo 1

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -75,9 +76,10 @@ public class HomeController {
 		return "home";
 	}
 	
-	@RequestMapping(value="/detail")
-	public String mostrarDetalle(Model model) 
+	@RequestMapping(value="/detail/{id}",  method=RequestMethod.GET)
+	public String mostrarDetalle(Model model, @PathVariable("id") int idPelicula) 
 	{
+		System.out.println("idPelicula: " + idPelicula);
 		String tituloPelicula  = "Rapidos y furiosos";
 		int duracion = 136;
 		double precioEntrada = 50;
--- webapp/WEB-INF/views/home.jsp
+++ webapp/WEB-INF/views/home.jsp
@@ -103,7 +103,7 @@
 							<span class="label label-default">${pelicula.genero }</span>
 						</h4>
 						<p>
-							<a class="btn btn-sm btn-primary" href="#" role="button">Consulta
+							<a class="btn btn-sm btn-primary" href="detail/${pelicula.id}" role="button">Consulta
 								Horarios &raquo;</a>
 						</p>
 					</div>
