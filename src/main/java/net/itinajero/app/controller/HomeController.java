e16ef570946d5aca6d4a68c6f02476f10350d5ff
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-07 13:29:04 -0500

    Initial commit

---
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -0,0 +1,90 @@
+package net.itinajero.app.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import net.itinajero.app.model.Pelicula;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	public String goHome(){
+		return "home";
+	}
+	//Metodo para generar una lista de Objetos de Modelo (Pelicula)
+	private List<Pelicula> getLista()
+	{
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		List<Pelicula> lista  = null;
+		try {
+			lista = new LinkedList<>();
+			Pelicula pelicula1 = new Pelicula();
+			pelicula1.setId(1);
+			pelicula1.setTitulo("Power Ranger");
+			pelicula1.setDuracion(120);
+			pelicula1.setClasificacion("B");
+			pelicula1.setGenero("Aventura");
+			pelicula1.setFechaEstreno(formatter.parse("02-05-2017"));
+			pelicula1.setEstatus("inactivo");
+			
+			Pelicula pelicula2 = new Pelicula();
+			pelicula2.setId(2);
+			pelicula2.setTitulo("La bella y la bestía");
+			pelicula2.setDuracion(132);
+			pelicula2.setClasificacion("A");
+			pelicula2.setGenero("Infantil");
+			pelicula2.setFechaEstreno(formatter.parse("20-05-2017"));
+			pelicula2.setImagen("bella.png");
+			
+			Pelicula pelicula3 = new Pelicula();
+			pelicula3.setId(3);
+			pelicula3.setTitulo("Contratiempo");
+			pelicula3.setDuracion(106);
+			pelicula3.setClasificacion("B");
+			pelicula3.setGenero("Thriller");
+			pelicula3.setFechaEstreno(formatter.parse("28-05-2017"));
+			pelicula3.setImagen("contratiempo.png");
+			
+			lista.add(pelicula1);
+			lista.add(pelicula2);
+			lista.add(pelicula3);
+			
+			return lista;
+		} catch (Exception e) {
+			// TODO: handle exception}
+			System.out.println("Error: "+ e.getMessage());
+			return null;
+		}
+		
+	}
+	@RequestMapping(value="/",  method=RequestMethod.GET)
+	public String mostrarPrincipal(Model model) 
+	{
+		List<Pelicula> peliculas = getLista();
+		//peliculas.add("Rapido y furiosos");
+		//peliculas.add("El aro 2");
+		//peliculas.add("Aliens");
+		model.addAttribute("peliculas", peliculas);
+		return "home";
+	}
+	
+	@RequestMapping(value="/detail")
+	public String mostrarDetalle(Model model) 
+	{
+		String tituloPelicula  = "Rapidos y furiosos";
+		int duracion = 136;
+		double precioEntrada = 50;
+		model.addAttribute("titulo", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precioEntrada);
+		return "detalle";
+	}
+	
+}
---
+++ webapp/WEB-INF/views/detalle.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Titulo de la pela Pelicula ${titulo}</h1>
+	<h2>Duracion ${duracion}</h2>
+	<h3>Precio ${precio}</h3>
+</body>
+</html>
