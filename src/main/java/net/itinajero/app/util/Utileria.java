6af4524bc6fb7c91d439305c5c257620a93a9113
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-10 11:47:02 -0500

    74. Upload Files - Agregar caracteres aleatorios al nombre del archivo

--- src/main/java/net/itinajero/app/util/Utileria.java
+++ src/main/java/net/itinajero/app/util/Utileria.java
@@ -34,11 +34,25 @@ public class Utileria {
 		return nextDays;
 	}
 	
+	public static String randomAlphaNumeric(int count) 
+	{
+		String CARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		StringBuilder builder = new StringBuilder();
+		while(count-- != 0) 
+		{
+			int character = (int) (Math.random() * CARACTERES.length());
+			builder.append(CARACTERES.charAt(character));
+		}
+		
+		return builder.toString();
+	}
+	
 	public static String guardarImagen(MultipartFile multiPart, HttpServletRequest request) {
 		// Obtenemos el nombre original del archivo
 		String nombreOriginal = multiPart.getOriginalFilename();
 		
 		nombreOriginal = nombreOriginal.replace(" ", "-");
+		String nombreFinal = randomAlphaNumeric(8)+nombreOriginal;
 		
 		
 		// Obtenemos la ruta ABSOLUTA del directorio images
@@ -46,10 +60,10 @@ public class Utileria {
 		String rutaFinal = request.getServletContext().getRealPath("/resources/images/");
 		try {
 			// Formamos el nombre del archivo para guardarlo en el disco duro
-			File imageFile = new File(rutaFinal + nombreOriginal);
+			File imageFile = new File(rutaFinal + nombreFinal);
 			// Aqui se guarda fisicamente el archivo en el disco duro
 			multiPart.transferTo(imageFile);
-			return nombreOriginal;
+			return nombreFinal;
 		} catch (IOException e) {
 			System.out.println("Error " + e.getMessage());
 			return null;
--- webapp/WEB-INF/views/includes/menu.jsp
+++ webapp/WEB-INF/views/includes/menu.jsp
@@ -11,7 +11,7 @@
 					class="icon-bar"></span> <span class="icon-bar"></span> <span
 					class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">My CineSite</a>
+			<a class="navbar-brand" href="${urlRoot}">My CineSite</a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
