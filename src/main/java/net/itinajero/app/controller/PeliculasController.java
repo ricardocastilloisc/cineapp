edb32e23a58f39052d705d8e6382ac3760b082f8
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-09 23:59:16 -0500

    61. BindingResult - Verificar errores

--- src/main/java/net/itinajero/app/controller/PeliculasController.java
+++ src/main/java/net/itinajero/app/controller/PeliculasController.java
@@ -2,6 +2,7 @@ package net.itinajero.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,16 @@ public class PeliculasController {
 	@PostMapping("/save")
 	public String guardar(Pelicula pelicula, BindingResult result) 
 	{
+		/*
+		if(result.hasErrors()) 
+		{
+			System.out.println("Existieron errores");
+			return "peliculas/formPelicula";
+		}*/
+		
+		for (ObjectError error: result.getAllErrors()){
+			System.out.println(error.getDefaultMessage());
+			}
 		System.out.println("Recibiendo objeto pelicula " + pelicula);
 		return "peliculas/formPelicula";
 	}
