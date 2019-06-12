6a41adda462553a4e7d2c3fd15436d8a2e0ad32a
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-12 17:09:17 -0500

    105. Anotación @Repository

--- src/main/java/net/itinajero/app/repository/NoticiasRepository.java
+++ src/main/java/net/itinajero/app/repository/NoticiasRepository.java
@@ -1,9 +1,11 @@
 package net.itinajero.app.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import net.itinajero.app.model.Noticia;
 
+@Repository
 public interface NoticiasRepository extends CrudRepository<Noticia, Integer> {
 
 }
