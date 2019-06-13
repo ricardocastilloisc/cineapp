ce4bc640156c303fb486299907741279cdd70fda
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-13 15:58:35 -0500

    114. Método findAllById - Recuperar varias entidades por Id

--- src/main/java/pruebasjpa/AppReadBuscarPorIds.java
+++ src/main/java/pruebasjpa/AppReadBuscarPorIds.java
@@ -1,22 +1,30 @@
 package pruebasjpa;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import net.itinajero.app.model.Noticia;
 import net.itinajero.app.repository.NoticiasRepository;
 
-public class AppCreate {
+public class AppReadBuscarPorIds {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		Noticia noticia = new Noticia();
-		
-		noticia.setTitulo("Proximo Estreno: Juego Macabr6666");
-		noticia.setDetalle("el mes de septiembre se estrena");
-		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
 		NoticiasRepository repo = context.getBean("noticiasRepository", NoticiasRepository.class);
-		repo.save(noticia);
+
+		List<Integer> ids = new LinkedList<Integer>();
+		ids.add(8);
+		ids.add(5);
+
+		Iterable<Noticia> it = repo.findAllById(ids);
+		for (Noticia n : it) {
+			System.out.println(n);
+		}
 		context.close();
 
 	}
