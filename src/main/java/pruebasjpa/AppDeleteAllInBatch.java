c874583b7f2ade494e3561c9f79ff2c090da3b3f
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-13 16:27:03 -0500

    117. Método deleteAllInBatch - Eliminar todos los registros en lote

---
+++ src/main/java/pruebasjpa/AppDeleteAllInBatch.java
@@ -0,0 +1,21 @@
+package pruebasjpa;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import net.itinajero.app.model.Noticia;
+import net.itinajero.app.repository.NoticiasRepository;
+
+public class AppDeleteAllInBatch {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+		NoticiasRepository repo = context.getBean("noticiasRepository", NoticiasRepository.class);
+		
+		repo.deleteAllInBatch();
+		context.close();
+
+	}
+
+}
