a3c27185be417cdb8620da0da30c789c9511ffbf
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-13 15:36:42 -0500

    111. Método count - Obtener número de entidades de una tabla

---
+++ src/main/java/pruebasjpa/AppCount.java
@@ -0,0 +1,18 @@
+package pruebasjpa;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import net.itinajero.app.repository.NoticiasRepository;
+
+public class AppCount {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+		NoticiasRepository repo = context.getBean("noticiasRepository", NoticiasRepository.class);
+		
+		long num  = repo.count();
+		System.out.println(num);
+			
+		context.close();
+	}
+
+}
