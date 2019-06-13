87940526f0069b50df6e83aa250c2b2415497f17
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-13 15:44:10 -0500

    113. Método deleteAll - Eliminar todos los registros

---
+++ src/main/java/pruebasjpa/AppDeleteAll.java
@@ -0,0 +1,19 @@
+package pruebasjpa;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import net.itinajero.app.repository.NoticiasRepository;
+
+public class AppDeleteAll {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+		NoticiasRepository repo = context.getBean("noticiasRepository", NoticiasRepository.class);
+		
+		repo.deleteAll();
+
+		context.close();
+
+	}
+}
