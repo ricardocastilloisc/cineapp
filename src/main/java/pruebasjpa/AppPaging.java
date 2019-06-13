03bca21e4b7ed530ebb0f7518cca71b1303944ec
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-13 16:51:53 -0500

    121. Método findAll con paginación y ordernamiento

--- src/main/java/pruebasjpa/AppPaging.java
+++ src/main/java/pruebasjpa/AppPaging.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import net.itinajero.app.model.Noticia;
 import net.itinajero.app.repository.NoticiasRepository;
@@ -15,7 +16,11 @@ public class AppPaging {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
 		NoticiasRepository repo = context.getBean("noticiasRepository", NoticiasRepository.class);
 		
-		Page<Noticia> page = repo.findAll(PageRequest.of(1, 10));
+		//Obtener todas las entidades por paginacion
+		//Page<Noticia> page = repo.findAll(PageRequest.of(1, 10));
+		
+		
+		Page<Noticia> page = repo.findAll(PageRequest.of(0, 10, Sort.by("titulo")));
 		
 		for(Noticia n: page) 
 		{
