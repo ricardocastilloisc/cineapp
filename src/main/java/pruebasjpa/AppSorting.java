0e296abf0c3122222633e33d40ede393d2391052
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-13 16:36:26 -0500

    118. Método findAll con ordenamiento (order by) - Ejemplo

---
+++ src/main/java/pruebasjpa/AppSorting.java
@@ -0,0 +1,26 @@
+package pruebasjpa;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.domain.Sort;
+
+import net.itinajero.app.model.Noticia;
+import net.itinajero.app.repository.NoticiasRepository;
+
+public class AppSorting {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+		NoticiasRepository repo = context.getBean("noticiasRepository", NoticiasRepository.class);
+		
+		List<Noticia> lista = repo.findAll(Sort.by("titulo").descending());
+		for(Noticia n: lista) 
+		{
+			System.out.println(n);
+		}
+		context.close();
+
+	}
+
+}
