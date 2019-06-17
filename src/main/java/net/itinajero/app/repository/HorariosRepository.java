72257af540705ed027720be609c182d670020aa8
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-17 17:40:14 -0500

    150. Desplegar Horarios por película y fecha - Parte 1

--- src/main/java/net/itinajero/app/repository/HorariosRepository.java
+++ src/main/java/net/itinajero/app/repository/HorariosRepository.java
@@ -1,9 +1,15 @@
 package net.itinajero.app.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.itinajero.app.model.Horario;
 
 public interface HorariosRepository extends JpaRepository<Horario, Integer> {
+	
+	// Horarios por idPelicula (Pelicula.id = Pelicula_Id and fecha=?)
+	public List<Horario> findByPelicula_IdAndFechaOrderByHora(int idPelicula,Date fecha);
 
 }
