ce272630d862d15a4ffa54e3e0202e102eb92d0b
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-17 18:01:18 -0500

    151. Desplegar Horarios por película y fecha - Parte 2

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -6,15 +6,20 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.itinajero.app.model.Horario;
 import net.itinajero.app.model.Pelicula;
 import net.itinajero.app.service.IBannersService;
+import net.itinajero.app.service.IHorariosService;
 import net.itinajero.app.service.IPeliculasService;
 import net.itinajero.app.util.Utileria;
 
@@ -26,6 +31,9 @@ public class HomeController {
 	
 	@Autowired
 	private IPeliculasService servicePeliculas;
+	
+	@Autowired
+	private IHorariosService serviceHorarios;
 
 	private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
@@ -65,20 +73,19 @@ public class HomeController {
 
 	@RequestMapping(value="/detail/{id}/{fecha}", method=RequestMethod.GET)
 	 public String mostrarDetalle(Model model, @PathVariable("id") int idPelicula,
-	@PathVariable("fecha") String fecha ){
-	//@RequestMapping(value = "/detail", method = RequestMethod.GET)
-	//public String mostrarDetalle(Model model, @RequestParam("idMovie") int idPelicula,
-		//	@RequestParam("fecha") String fecha) {
-		System.out.println("idPelicula: " + idPelicula);
-		System.out.println("Para la fecha: " + fecha);
+	@PathVariable("fecha") Date fecha ){
+		List<Horario> horarios = serviceHorarios.buscarPorIdPelicula(idPelicula, fecha);
+		model.addAttribute("horarios", horarios);
+		model.addAttribute("fechaBusqueda", dateFormat.format(fecha));
 		model.addAttribute("pelicula", servicePeliculas.buscarPorId(idPelicula));
-		/*
-		 * String tituloPelicula = "Rapidos y furiosos"; int duracion = 136; double
-		 * precioEntrada = 50; model.addAttribute("titulo", tituloPelicula);
-		 * model.addAttribute("duracion", duracion); model.addAttribute("precio",
-		 * precioEntrada);
-		 */
 		return "detalle";
+	}
+	
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
 	}
 
 }
