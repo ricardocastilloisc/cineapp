ecf6ba00d0044ade3f36496a851643cc5fb4255b
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-17 10:38:29 -0500

    139. Anotación @Autowired - Inyectar Repositorio JPA en una clase de servi…

---
+++ src/main/java/net/itinajero/app/service/PeliculasServiceJPA.java
@@ -0,0 +1,42 @@
+package net.itinajero.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.itinajero.app.model.Pelicula;
+import net.itinajero.app.repository.PeliculasRepository;
+
+@Service
+public class PeliculasServiceJPA implements IPeliculasService {
+	
+	@Autowired
+	private PeliculasRepository peliculasRepo;
+
+	@Override
+	public void insertar(Pelicula pelicula) {
+		
+		
+		
+	}
+
+	@Override
+	public List<Pelicula> buscarTodas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pelicula buscarPorId(int idPelicula) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> buscarGeneros() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
