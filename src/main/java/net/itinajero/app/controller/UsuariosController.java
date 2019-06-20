9f669afbbd6c17be11d28431272621813c2c975b
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-20 17:39:21 -0500

    184. Recursos - Código fuente de CINEAPP con seguridad (Spring Sec…

--- src/main/java/net/itinajero/app/controller/UsuariosController.java
+++ src/main/java/net/itinajero/app/controller/UsuariosController.java
@@ -4,7 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.itinajero.app.model.Perfil;
+import net.itinajero.app.model.Usuario;
+import net.itinajero.app.service.IPerfilesService;
+import net.itinajero.app.service.IUsuariosService;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -12,6 +20,45 @@ public class UsuariosController {
 	 
 	@Autowired
 	private BCryptPasswordEncoder encoder;
+	
+	@Autowired
+	private IUsuariosService serviceUsuarios;
+	
+	@Autowired
+	private IPerfilesService servicePerfiles;
+	
+	
+	@GetMapping("/create")
+	public String crear(@ModelAttribute Usuario usuario) 
+	{
+		return "usuarios/formUsuario";
+	}
+	
+	@GetMapping("/index")
+	public String index() 
+	{
+		return "usuarios/listUsuarios";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(@ModelAttribute Usuario usuario, @RequestParam("perfil") String perfil) 
+	{
+		System.out.println(usuario);
+		System.out.println(perfil);
+		
+		String tmpPass = usuario.getPwd();
+		String encriptado = encoder.encode(tmpPass);
+		usuario.setPwd(encriptado);
+		usuario.setActivo(1);
+		serviceUsuarios.guardar(usuario);
+		
+		Perfil perfilTmp = new Perfil();
+		perfilTmp.setCuenta(usuario.getCuenta());
+		perfilTmp.setPerfil(perfil);
+		servicePerfiles.guardar(perfilTmp);
+		
+		return "redirect:/usuarios/index";
+	}
 	
 	@GetMapping("/demo-bcrypt")
 	public String pruebaBCrypt() 
